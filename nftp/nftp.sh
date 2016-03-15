ffe4b4cfa04ab972b64c45553452b9b01579e97d
Tyler Harris <tylerdavidharris@gmail.com>
2016-03-15 12:07:40 -0600

    fix

--- nftp/nftp.sh
+++ nftp/nftp.sh
@@ -12,37 +12,37 @@ rm `find $logDirectory -name 'history.log' -size +1M` 2>/dev/null
 
 if [ "$(ls -A $outgoingDirectory)" ]; then
 
-	# Get Contents of specified directory
-	echo -e "Retrieving local listing from $outgoingDirectory..."
-	ls $outgoingDirectory/ | fold > $logDirectory/localList
-	
-	# Get current list on ftp & cleanup (remove ./ characters)
-	echo -e "\nRetrieving remote listing from $ftpServer:/$ftpServerDirectory..."
-	lftp "$lftpBookmark" -e "cd $ftpServerDirectory; ls; bye" > $logDirectory/ftpList	
-	cat $logDirectory/ftpList | sed -e 's/.\///g' > $logDirectory/ftpList	
-	
-	# Compare ftp contents with contents
-	echo -e "\nDetermining an upload list..."
-	grep -Fxvf $logDirectory/ftpList $logDirectory/localList > $logDirectory/diffList
+        # Get Contents of specified directory
+        echo -e "Retrieving local listing from $outgoingDirectory..."
+        ls $outgoingDirectory/ | fold > $logDirectory/localList
 
-	diffList=`cat $logDirectory/diffList 2>/dev/null`
-	fileArray=($diffList);
+        # Get current list on ftp & cleanup (remove ./ characters)
+        echo -e "\nRetrieving remote listing from $ftpServer:/$ftpServerDirectory..."
+        lftp "$lftpBookmark" -e "cd $ftpServerDirectory; ls; bye" > $logDirectory/ftpList
+        cat $logDirectory/ftpList | awk '{ print $9 }' > $logDirectory/parsedFtpList
 
-	# Upload missing files to FTP server
-	if [[ ! -z ${fileArray[@]} ]]; then
-		echo -e "\nUploading files to ftp..."
-		cd $outgoingDirectory
-		echo -e "\n${fileArray[@]}\n"
+        # Compare ftp contents with contents
+        echo -e "\nDetermining an upload list..."
+        grep -Fxvf $logDirectory/parsedFtpList $logDirectory/localList > $logDirectory/diffList
+
+        diffList=`cat $logDirectory/diffList 2>/dev/null`
+        fileArray=($diffList);
+	echo "$fileArray"
+        # Upload missing files to FTP server
+        if [[ ! -z ${fileArray[@]} ]]; then
+                echo -e "\nUploading files to ftp..."
+                cd $outgoingDirectory
+                echo -e "\n${fileArray[@]}\n"
 
 lftp "$lftpBookmark" <<EOF
 cd $ftpServerDirectory
-mput ${fileArray[@]}; 
+mput ${fileArray[@]};
 bye
 EOF
-	echo -e `date` "Files uploaded: "${fileArray[@]} >> $logDirectory/$logFilename
-	else echo -e `date` "Nothing to upload." >> $logDirectory/$logFilename; echo -e "Nothing to upload."
-	fi
+        echo -e `date` "Files uploaded: "${fileArray[@]} >> $logDirectory/$logFilename
+        else echo -e `date` "Nothing to upload." >> $logDirectory/$logFilename; echo -e "Nothing to upload."
+        fi
 else echo -e `date` "No files in outgoing directory." >> $logDirectory/$logFilename
 fi
-rm -f $logDirectory/localList $logDirectory/ftpList $logDirectory/diffList
+rm -f $logDirectory/localList $logDirectory/ftpList $logDirectory/parsedFtpList $logDirectory/diffList
 exit 0
