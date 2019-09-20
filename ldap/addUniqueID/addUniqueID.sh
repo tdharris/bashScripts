6bb32c7fc11a32aec3fff662127eb918af17476b
Tyler Harris <tdharris@users.noreply.github.com>
2019-09-20 16:10:33 -0600

    Update addUniqueID.sh

--- ldap/addUniqueID/addUniqueID.sh
+++ ldap/addUniqueID/addUniqueID.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Detect missing uniqueID attribute via ldap, created ldif and prompts for automatic import to resolve.
+# Created by Tyler Harris (github.com/tdharris)
+
 rm -f userList fixed.ldf
 function askYesOrNo {
 	REPLY=""
